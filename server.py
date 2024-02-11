9886bd7c16d5e98ffd3e141e49f934b33e435652
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-02-12 00:10:32 +0530

    fixed glitch in server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def my_home(): 
-    return render_template("index.html")
+    return render_template("index.html") 
 
 @app.route('/<string:page_name>')
 def html_pages(page_name):
